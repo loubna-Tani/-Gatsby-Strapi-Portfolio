87920a64669660737344c2b1200623b8ede1a24b
loubna-tani <49785408+loubnaben@users.noreply.github.com>
2020-10-28 17:09:05 -0500

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import SocialLinks from "../constants/socialLinks"
+// ...GatsbyImageSharpFluid
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+const Hero = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Loubna</h1>
+            <h4>Front End Web Developer</h4>
+            <Link to ="/contact" className="btn"> contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
+}
+
+export default Hero
+
